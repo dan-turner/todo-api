94cc69a308114f0c931fb6a8aaaada9eb950988a
Dan Turner <dan.p.turner@gmail.com>
2017-04-21 14:36:45 +1000

    Add graphiql

--- handlers/graphql.js
+++ handlers/graphql.js
@@ -1,10 +1,14 @@
 'use strict';
 
-import { graphqlLambda } from 'graphql-server-lambda';
+import { graphqlLambda, graphiqlLambda } from 'graphql-server-lambda';
 import { schema } from '../graphql';
 
 export const graphql = graphqlLambda({
   schema: schema,
   logFunction: console.log,
   debug: true
+});
+
+export const graphiql = graphiqlLambda({
+    endpointURL: '/dev/graphql'
 });
